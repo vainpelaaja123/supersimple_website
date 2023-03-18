a5948b939797e4c5558c2164dca17e03bbadd1cb
vainpelaaja123 <128252376+vainpelaaja123@users.noreply.github.com>
2023-03-19 01:40:45 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,45 @@
+function addTime() {
+    // Get the current time without seconds
+    var currentTime = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
 
+    // Check if the time is already in the list
+    var timeList = document.getElementById("timeList");
+    var times = timeList.getElementsByTagName("li");
+    var alreadyExists = false;
+    for (var i = 0; i < times.length; i++) {
+        if (times[i].textContent === currentTime) {
+            alreadyExists = true;
+            break;
+        }
+    }
+
+    // Add the time to the list if it doesn't already exist
+    if (!alreadyExists) {
+        var newTime = document.createElement("li");
+        newTime.textContent = currentTime;
+
+        // Find the correct position for the new item
+        var inserted = false;
+        for (var i = 0; i < times.length; i++) {
+            if (currentTime < times[i].textContent) {
+                timeList.insertBefore(newTime, times[i]);
+                inserted = true;
+                break;
+            }
+        }
+
+        // If the new item is the latest, append it to the end
+        if (!inserted) {
+            timeList.appendChild(newTime);
+        }
+
+        // Save the times to Local Storage
+        localStorage.setItem("times", timeList.innerHTML);
+    }
+}
+
+window.onload = function() {
+    // Load the times from Local Storage
+    var timeList = document.getElementById("timeList");
+    timeList.innerHTML = localStorage.getItem("times");
+};
